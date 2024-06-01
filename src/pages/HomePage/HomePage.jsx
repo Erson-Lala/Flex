88ca40a019967eb93e3d8bcd721e78bbe832eb34
Erson Lala <erson.lala111@gmail.com>
2024-06-01 18:10:31 +0200

    Added more jobs and maxwidth

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -8,6 +8,20 @@ function HomePage() {
     const [jobSites, setJobSites] = useState([
         { name: '1658 E 23rd St, Brooklyn, NY 11229, USA', status: 'Completed' },
         { name: '1705 E 22nd St, Brooklyn, NY 11229, USA', status: 'On Hold' },
+        { name: '123 Main St, New York, NY 10001, USA', status: 'On Road' },
+        { name: '456 Maple Ave, Queens, NY 11101, USA', status: 'Completed' },
+        { name: '789 Elm St, Bronx, NY 10451, USA', status: 'On Hold' },
+        { name: '987 Oak St, Staten Island, NY 10301, USA', status: 'On Road' },
+        { name: '654 Pine St, Brooklyn, NY 11230, USA', status: 'Completed' },
+        { name: '321 Birch St, Manhattan, NY 10002, USA', status: 'On Hold' },
+        { name: '213 Cedar St, Brooklyn, NY 11232, USA', status: 'On Road' },
+        { name: '678 Spruce St, Queens, NY 11432, USA', status: 'Completed' },
+        { name: '876 Ash St, Bronx, NY 10457, USA', status: 'On Hold' },
+        { name: '543 Walnut St, Staten Island, NY 10304, USA', status: 'On Road' },
+        { name: '432 Chestnut St, Brooklyn, NY 11208, USA', status: 'Completed' },
+        { name: '987 Willow St, Manhattan, NY 10027, USA', status: 'On Hold' },
+        { name: '321 Poplar St, Brooklyn, NY 11206, USA', status: 'On Road' },
+        { name: '654 Cypress St, Queens, NY 11385, USA', status: 'Completed' }
     ]);
 
     const [isModalOpen, setModalOpen] = useState(false);
